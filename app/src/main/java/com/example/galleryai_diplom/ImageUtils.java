38225a9e535d522b7bd97359e66f252ce4be6650
alan <pancheno@bk.ru>
2025-02-18 01:09:08 +0700

    first_version

---
+++ app/src/main/java/com/example/galleryai_diplom/ImageUtils.java
@@ -0,0 +1,27 @@
+package com.example.galleryai_diplom;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImageUtils {
+    public static List<String> loadImages(Context context) {
+        List<String> imagePaths = new ArrayList<>();
+        Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {MediaStore.Images.Media.DATA};
+
+        Cursor cursor = context.getContentResolver().query(uri, projection, null, null, null);
+        if (cursor != null) {
+            int columnIndex = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            while (cursor.moveToNext()) {
+                imagePaths.add(cursor.getString(columnIndex));
+            }
+            cursor.close();
+        }
+        return imagePaths;
+    }
+}
